fc26afa0d29c2e3f2dca8cff45f1a39f7feac21a
Yitshak770 <96015515+Yitshak770@users.noreply.github.com>
2022-02-01 21:40:05 +0200

    2eme envoi du 1/2/2022

--- JS1/script/catch-me.js
+++ JS1/script/catch-me.js
@@ -1,8 +1,5 @@
-/**************************** Mode d'emploi et tests DEBUT *********************************************/
-document.querySelector(".test").addEventListener("click", test);
-function test() {
+/**************************** Mode d'emploi DEBUT *********************************************/
 
-}
 let manualButton = document.querySelector(".manual")
 manualButton.addEventListener("click", manual);
 function manual() {
@@ -11,18 +8,18 @@ function manual() {
 }
 
 
-/**************************** Mode d'emploi et tests FIN *********************************************/
+/**************************** Mode d'emploi FIN *********************************************/
 
 /**************************** cliquage sur le titre DEBUT *********************************************/
 let myH1 = document.querySelector("#titre h1");
 myH1.addEventListener("mouseenter", myHover);
 function myHover() {
     myH1.innerHTML = " Do you want to start ?";
+    document.querySelector("#titre h1").addEventListener("mouseleave", function() {
+        this.innerHTML = " CATCH ME IF YOU CAN";
+    });
 };
 
-document.querySelector("#titre h1").addEventListener("mouseleave", function() {
-    this.innerHTML = " CATCH ME IF YOU CAN";
-});
 /**************************** cliquage sur le titre FIN *********************************************/
 
 
@@ -33,10 +30,10 @@ const C = {
     mn : {val : 60 , DOM : document.getElementById("mn")},
     sec : {val : 60 , DOM : document.getElementById("sec")}
 };
-
+let temps = 1;
 function myTimer() {
     const departMinutes = 60
-    let temps = departMinutes * 60
+    temps = departMinutes * 60
     setInterval(function() {
         let mn = parseInt(temps / 60, 10)
         let sec = parseInt(temps % 60, 10)
@@ -50,10 +47,9 @@ function myTimer() {
         }  else {
             temps --
         }
-    }, 1)
+    }, 100);
+    return temps;
 }
-
-
 ///////////////////////////////FONCTION TIMER FIN
 
 
@@ -61,7 +57,11 @@ function myTimer() {
 ///////////////////////////////FONCTION JEU TERMINE DEBUT
 function endGame () {
     if (C.mn.DOM.innerHTML == 00 && C.sec.DOM.innerHTML == 00){
-        document.querySelector(".catchme").classList.remove("rotating");
+        myCatch.classList.remove("rotating");
+        myCatch.classList.remove("rotating-100");
+        myCatch.classList.remove("rotating-125");
+        myCatch.classList.remove("rotating-150");
+        myCatch.classList.remove("rotating-175");
     };
     document.querySelector("#ecran H3").innerHTML = " GAME OVER ";
     document.querySelector("#ecran H3").style.display = "block";
@@ -70,6 +70,8 @@ function endGame () {
     stars.forEach (function(x){
         x.classList.add("show");
     });
+    myEcran.removeEventListener("click", myClicks);
+    myCatch.removeEventListener("click", myWins)
     
 }
 ///////////////////////////////FONCTION JEU TERMINE FIN
@@ -82,8 +84,9 @@ const height = myEcran.clientHeight;
 const width = myEcran.clientWidth;
 let myCatch = document.querySelector("#catchme");
 // myCatch.addEventListener("mouseover", catchMove);
+let nbr = 300;
 function catchMove() {
-    setTimeout(myMove, 300);
+    setTimeout(myMove, nbr);
     function myMove() {
         let randY = Math.floor((Math.random() * height * 83/100) );
         let randX = Math.floor((Math.random() * width * 83/100) );
@@ -113,8 +116,8 @@ let clickCounter = 0;
 
 
 //----- Mode de calcul du score et des clicks perdus DEBUT -----//
-myEcran.addEventListener("click", myClicks);
-myCatch.addEventListener("click", myWins)
+// myEcran.addEventListener("click", myClicks);
+// myCatch.addEventListener("click", myWins)
 function myWins() {
     myWinCpt = 10*myLevelCpt
 }
@@ -133,25 +136,35 @@ function myClicks() {
     //chaque 10 clics on augmente de niveau
     //chaque niveau : + 10 au timer | 0.25 sec en moins dans vitesse rotation | 50 ms en moins dans le timeout 
     clickCounter ++;
+    
+    
     switch (true) {
         case (clickCounter > 49) :
             endGame()
             break;
-        case (clickCounter > 39) :
+        case (clickCounter > 39 && clickCounter <41) :
             myLevelCpt = 5;
-            //temps = temps + 10;
+            nbr = 100;
+            myCatch.classList.add("rotating-100");
+            temps = temps + 600;
             break;
-        case (clickCounter > 29) :
+        case (clickCounter > 29 && clickCounter <31) :
             myLevelCpt = 4;
-            //temps = temps + 10;
+            nbr = 150;
+            myCatch.classList.add("rotating-125");
+            temps = temps + 600;
             break;
-        case (clickCounter > 19) :
+        case (clickCounter > 19 && clickCounter <21) :
             myLevelCpt = 3;
-            //temps = temps + 10;
+            nbr = 200;
+            myCatch.classList.add("rotating-150");
+            temps = temps + 600;
             break;
-        case (clickCounter > 9) :
+        case (clickCounter > 9 && clickCounter < 11) :
             myLevelCpt = 2;
-            //temps = temps + 10;
+            nbr = 250;
+            myCatch.classList.add("rotating-175");
+            temps = temps + 600;
             break;
         default:
             break;
@@ -187,9 +200,34 @@ function startGame() {
     myDisplays.forEach (function(x) {
         x.style.display="block";
     }); 
-    document.querySelector(".catchme").classList.add("rotating");
+    myCatch.classList.add("rotating");
     myCatch.addEventListener("mouseover", catchMove);  
     myTimer();
+    myEcran.addEventListener("click", myClicks);
+    myCatch.addEventListener("click", myWins)
 };
 ///////////////////////////////FONCTION DEPART DU JEU FIN
 /**************************** ouverture de l'alerte et départ du jeu FIN *********************************************/
+
+/**************************** new game DEBUT *********************************************/
+document.querySelector(".new-game").addEventListener("click", newGame);
+function newGame() {
+    //clearInterval
+    //temps = 3600;
+    document.querySelector("#ecran H3").innerHTML = "CLICK ON TITLE TO BEGIN";
+    let stars = document.querySelectorAll(".stars");
+    stars.forEach (function(x){
+        x.classList.remove("show");
+    });
+    myHover()
+    myH1.addEventListener("mouseenter", myHover);
+    myH1.addEventListener("click", startGame);
+
+
+    // myWinCpt = 0;
+    // myScoreCpt = 0;
+    // myLevelCpt = 1;
+    // myMissedClicksCpt = 0;
+    // clickCounter = 0;
+}
+/**************************** new game DEBUT *********************************************/
