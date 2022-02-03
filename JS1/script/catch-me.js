bdbf2a1b456207272941ce4ea4dba29d3c659377
Yitshak770 <96015515+Yitshak770@users.noreply.github.com>
2022-02-04 00:12:26 +0200

    envoi du 03/02/2022

--- JS1/script/catch-me.js
+++ JS1/script/catch-me.js
@@ -1,16 +1,15 @@
 /**************************** Mode d'emploi DEBUT *********************************************/
-
 let manualButton = document.querySelector(".manual")
 manualButton.addEventListener("click", manual);
 function manual() {
-    document.querySelector(".manual-text").classList.toggle("show");
-    
+    document.querySelector(".manual-text").classList.toggle("show"); 
 }
-
-
 /**************************** Mode d'emploi FIN *********************************************/
 
-/**************************** cliquage sur le titre DEBUT *********************************************/
+
+
+
+/**************************** cliquages DEBUT *********************************************/
 let myH1 = document.querySelector("#titre h1");
 myH1.addEventListener("mouseenter", myHover);
 function myHover() {
@@ -19,42 +18,181 @@ function myHover() {
         this.innerHTML = " CATCH ME IF YOU CAN";
     });
 };
+myH1.addEventListener("click", startGame);
+document.querySelector(".new-game").addEventListener("click", newGame);
+/**************************** cliquage FIN *********************************************/
 
-/**************************** cliquage sur le titre FIN *********************************************/
 
 
 
-
-///////////////////////////////FONCTION TIMER DEBUT
+/**************************** timer chrono DEBUT *********************************************/
+let myTest = 0;
+let temps = 0;
+let chrono = null;
 const C = {
     mn : {val : 60 , DOM : document.getElementById("mn")},
-    sec : {val : 60 , DOM : document.getElementById("sec")}
+    sec : {val : 00 , DOM : document.getElementById("sec")}
 };
-let temps = 1;
-function myTimer() {
-    const departMinutes = 60
-    temps = departMinutes * 60
-    setInterval(function() {
-        let mn = parseInt(temps / 60, 10)
-        let sec = parseInt(temps % 60, 10)
-        mn = mn < 10 ? "0" + mn : mn
-        sec = sec < 10 ? "0" + sec : sec
-        C.mn.DOM.innerHTML = `${mn}`
-        C.sec.DOM.innerHTML = `${sec}`
-        if (temps <= 0) {
-            temps = 0;
-            endGame();
-        }  else {
-            temps --
-        }
-    }, 100);
-    return temps;
+C.mn.DOM.innerHTML = "60";
+C.sec.DOM.innerHTML = "00";
+///////////////////////////////FONCTION TIMER DEBUT
+function myInterval() {
+    let minute = parseInt(temps / 60)
+    let second = parseInt(temps % 60)
+    minute = minute < 10 ? "0" + minute : minute
+    second = second < 10 ? "0" + second : second
+    C.mn.DOM.innerHTML = `${minute}`
+    C.sec.DOM.innerHTML = `${second}`
+    if (temps <= 0) {
+        temps = 0;
+        myTest = 11;
+        endGame();
+    }  else {
+        temps --
+    }
+    return myTest;
+};
+function myTimer() {   
+    if (myTest === 55) {
+        chrono = setInterval(myInterval, 1); //17
+    };
 }
 ///////////////////////////////FONCTION TIMER FIN
+/**************************** timer chrono FIN *********************************************/
 
 
+
+
+
+
+
+/**************************** déplacement du catchme DEBUT *********************************************/
+let myEcran = document.querySelector("#ecran")
+const height = myEcran.clientHeight;
+const width = myEcran.clientWidth;
+let myCatch = document.querySelector("#catchme");
+
+let nbr = 350;            ////// vitesse de reotation du catchme
+function catchMove() {
+    setTimeout(myMove, nbr);
+    function myMove() {
+        let randY = Math.floor((Math.random() * height * 83/100) );
+        let randX = Math.floor((Math.random() * width * 83/100) );
+        myCatch.style.right = `${randX}px`;
+        myCatch.style.top = `${randY}px`;
+        // myCatch.style.transform = `translate(${randX}px, ${randY}px)`;
+    }
+};
+/**************************** déplacement du catchme FIN *********************************************/
+
+
+
+
+
+/**************************** décompte des points DEBUT *********************************************/
+let myScore = document.querySelector("#controles .datas");
+let myPoints = document.querySelector("#points .datas");
+let myLevel = document.querySelector("#level .datas");
+let myMissedClicks = document.querySelector("#clicks .datas");
+
+let myWinCpt = 0;
+let myScoreCpt = 0;
+let myLevelCpt = 1;
+let myMissedClicksCpt = 0;
+// let clickCounter = 0;
+let clickWinCounter = 0;
+let myPointsCpt = 10;
+
+myPoints.innerHTML = `${myPointsCpt}`;
+myLevel.innerHTML = `${myLevelCpt}`;
+myScore.innerHTML = `${myScoreCpt}`; 
+myMissedClicks.innerHTML = `${myMissedClicksCpt}`;
+
+//----- Mode de calcul du score et des clicks perdus DEBUT -----//
+function myWins() {
+    myWinCpt = 10*myLevelCpt
+    clickWinCounter ++;
+    myPointsCpt --;
+    if (myPointsCpt < 1){myPointsCpt = 10};
+    myPoints.innerHTML = `${myPointsCpt}`;
+}
+function myClicks() {
+    if (myWinCpt == 10 || myWinCpt == 20 || myWinCpt == 30 || myWinCpt == 40 || myWinCpt == 50) {
+        myScoreCpt = myScoreCpt + myWinCpt;
+        myWinCpt = 0;
+    } else {
+        myScoreCpt = myScoreCpt -1*myLevelCpt;
+        myMissedClicksCpt ++;
+        myMissedClicks.innerHTML = `${myMissedClicksCpt}`;
+    }
+    myScore.innerHTML = `${myScoreCpt}`;   
+    //----- gestion des niveaux DEBUT -----//
+    //chaque 10 clics gagnant on augmente de niveau
+    //chaque niveau : + 10 au timer | 0.25 sec en moins dans vitesse rotation | 50 ms en moins dans le timeout  
+    switch (true) {
+        case (clickWinCounter > 49) :
+            endGame()
+            break;
+        case (clickWinCounter > 39 && clickWinCounter <41) :
+            myLevelCpt = 5;
+            nbr = 150;
+            myCatch.classList.add("rotating-100");
+            temps = temps + 600;
+            break;
+        case (clickWinCounter > 29 && clickWinCounter <31) :
+            myLevelCpt = 4;
+            nbr = 200;
+            myCatch.classList.add("rotating-125");
+            temps = temps + 600;
+            break;
+        case (clickWinCounter > 19 && clickWinCounter <21) :
+            myLevelCpt = 3;
+            nbr = 250;
+            myCatch.classList.add("rotating-150");
+            temps = temps + 600;
+            break;
+        case (clickWinCounter > 9 && clickWinCounter < 11) :
+            myLevelCpt = 2;
+            nbr = 300;
+            myCatch.classList.add("rotating-175");
+            temps = temps + 600;
+            break;
+    }
+    myLevel.innerHTML = `${myLevelCpt}`;                        
+    //----- gestion des niveaux FIN -----//                          
+};
+//----- Mode de calcul du score et des clicks perdus FIN -----//
+/**************************** décompte des points FIN *********************************************/
+                        
+                        
+                        
+                        
+                        
+/**************************** départ et fin du jeu DEBUT *********************************************/
+///////////////////////////////FONCTION DEPART DU JEU DEBUT
+function startGame() {    
+    alert("Are you ready ?");
+    temps = 3600;
+    myTest = 55;
+    myH1.removeEventListener("mouseenter", myHover);
+    myH1.removeEventListener("click", startGame);
+    document.querySelector("#ecran H3").style.display = "none";
+    let myDisplays = document.querySelectorAll(".datas");
+    myDisplays.forEach (function(x) {
+        x.style.display="block";
+    }); 
+    myCatch.classList.add("rotating");
+    myCatch.addEventListener("mouseover", catchMove);  
+    myTimer();
+    myEcran.addEventListener("click", myClicks);
+    myCatch.addEventListener("click", myWins)
+};
+///////////////////////////////FONCTION DEPART DU JEU FIN
 
 ///////////////////////////////FONCTION JEU TERMINE DEBUT
+let hisName = "noname";
+let hisPoints = 0;
+let playerArr = [];
 function endGame () {
     if (C.mn.DOM.innerHTML == 00 && C.sec.DOM.innerHTML == 00){
         myCatch.classList.remove("rotating");
@@ -71,149 +209,101 @@ function endGame () {
         x.classList.add("show");
     });
     myEcran.removeEventListener("click", myClicks);
-    myCatch.removeEventListener("click", myWins)
+    myCatch.removeEventListener("click", myWins);
+    clearInterval(chrono);
+    hisName = prompt("what is your name ?").toLowerCase();
+    hisName = hisName.slice(0,1).toUpperCase() + hisName.slice(1);  
+    
+    playerArr = JSON.parse(localStorage.allPlayers);    // récupère le array des joueurs
+    hisPoints = myScoreCpt - myMissedClicksCpt;             // totalise ses points
+    
+    let newPlayer = new player(hisName, hisPoints, new Date().getTime());   
+    
+    playerArr.push(newPlayer); // ajoute le nouveau joueur dans le array des joueurs
+    
+    
+    playerArr.sort((a, b) => {                              // trie le array des joueurs
+        return b.hisPoints - a.hisPoints;
+    });
+    playerArr.pop();                                        // enlève le dernier joueur
+    localStorage.allPlayers = JSON.stringify(playerArr); // envoie le tableau à jour vers le local storage
+    afficheWinners();
     
 }
 ///////////////////////////////FONCTION JEU TERMINE FIN
+/**************************** départ et fin du jeu FIN *********************************************/
 
 
-/**************************** déplacement du catchme DEBUT *********************************************/
-
-let myEcran = document.querySelector("#ecran")
-const height = myEcran.clientHeight;
-const width = myEcran.clientWidth;
-let myCatch = document.querySelector("#catchme");
-// myCatch.addEventListener("mouseover", catchMove);
-let nbr = 300;
-function catchMove() {
-    setTimeout(myMove, nbr);
-    function myMove() {
-        let randY = Math.floor((Math.random() * height * 83/100) );
-        let randX = Math.floor((Math.random() * width * 83/100) );
-        myCatch.style.right = `${randX}px`;
-        myCatch.style.top = `${randY}px`;
-        // myCatch.style.transform = `translate(${randX}px, ${randY}px)`;
-    }
-};
-
-/**************************** déplacement du catchme FIN *********************************************/
 
 
-/**************************** décompte des points DEBUT *********************************************/
+/**************************** hight scores 5 premiers DEBUT *********************************************/
+let today = new Date();
+let myDate = Intl.DateTimeFormat("fr").format(new Date());     // format de date française          
+let myDateNbr = new Date().getTime();                          // format date numérique
+// console.log (myDate);
+// console.log (myDateNbr);
 
 
-let myScore = document.querySelector("#controles .datas");
-let myPoints = document.querySelector("#points .datas");
-let myLevel = document.querySelector("#level .datas");
-let myMissedClicks = document.querySelector("#clicks .datas");
+playerArr = JSON.parse(localStorage.allPlayers);    // récupère le array des joueurs
+afficheWinners();
 
-
-let myWinCpt = 0;
-let myScoreCpt = 0;
-let myLevelCpt = 1;
-let myMissedClicksCpt = 0;
-let clickCounter = 0;
-
-
-//----- Mode de calcul du score et des clicks perdus DEBUT -----//
-// myEcran.addEventListener("click", myClicks);
-// myCatch.addEventListener("click", myWins)
-function myWins() {
-    myWinCpt = 10*myLevelCpt
+function afficheWinners() {
+    fisrtWinnerScore = document.querySelector("#first .score-winner p");
+    fisrtWinnerName = document.querySelector("#first h6");
+    fisrtWinnerScore.innerHTML = playerArr[0].hisPoints;
+    fisrtWinnerName.innerHTML = playerArr[0].hisName;
+    
+    secondWinnerScore = document.querySelector("#second .score-winner p");
+    secondWinnerName = document.querySelector("#second h6");
+    secondWinnerScore.innerHTML = playerArr[1].hisPoints;
+    secondWinnerName.innerHTML = playerArr[1].hisName;
+    
+    thirdWinnerScore = document.querySelector("#third .score-winner p");
+    thirdWinnerName = document.querySelector("#third h6");
+    thirdWinnerScore.innerHTML = playerArr[2].hisPoints;
+    thirdWinnerName.innerHTML = playerArr[2].hisName;
+    
+    fourthWinnerScore = document.querySelector("#fourth .score-winner p");
+    fourthWinnerName = document.querySelector("#fourth h6");
+    fourthWinnerScore.innerHTML = playerArr[3].hisPoints;
+    fourthWinnerName.innerHTML = playerArr[3].hisName;
+    
+    fifthWinnerScore = document.querySelector("#fifth .score-winner p");
+    fifthWinnerName = document.querySelector("#fifth h6");
+    fifthWinnerScore.innerHTML = playerArr[4].hisPoints;
+    fifthWinnerName.innerHTML = playerArr[4].hisName;
 }
-function myClicks() {
-    if (myWinCpt == 10) {
-        myScoreCpt = myScoreCpt + myWinCpt;
-        myWinCpt = 0;
-    } else {
-        myScoreCpt = myScoreCpt -1*myLevelCpt;
-        myMissedClicksCpt = myMissedClicksCpt + 1;
-        myMissedClicks.innerHTML = `${myMissedClicksCpt}`;
-    }
-    myScore.innerHTML = `${myScoreCpt}`;
-    
-    //----- gestion des niveaux DEBUT -----//
-    //chaque 10 clics on augmente de niveau
-    //chaque niveau : + 10 au timer | 0.25 sec en moins dans vitesse rotation | 50 ms en moins dans le timeout 
-    clickCounter ++;
-    
-    
-    switch (true) {
-        case (clickCounter > 49) :
-            endGame()
-            break;
-        case (clickCounter > 39 && clickCounter <41) :
-            myLevelCpt = 5;
-            nbr = 100;
-            myCatch.classList.add("rotating-100");
-            temps = temps + 600;
-            break;
-        case (clickCounter > 29 && clickCounter <31) :
-            myLevelCpt = 4;
-            nbr = 150;
-            myCatch.classList.add("rotating-125");
-            temps = temps + 600;
-            break;
-        case (clickCounter > 19 && clickCounter <21) :
-            myLevelCpt = 3;
-            nbr = 200;
-            myCatch.classList.add("rotating-150");
-            temps = temps + 600;
-            break;
-        case (clickCounter > 9 && clickCounter < 11) :
-            myLevelCpt = 2;
-            nbr = 250;
-            myCatch.classList.add("rotating-175");
-            temps = temps + 600;
-            break;
-        default:
-            break;
-    }
-    myLevel.innerHTML = `${myLevelCpt}`;
-    
-    
-    //----- gestion des niveaux FIN -----//
-    
-    
-    
 
+fisrtWinnerName.addEventListener("mouseover", displayDate);
+function displayDate() {
 
-
+    alert("toto")
 };
 
-//----- Mode de calcul du score et des clicks perdus FIN -----//
-/**************************** décompte des points FIN *********************************************/
+
+class player {
+    constructor(_hisName, _hisPoints, _hisDate) {
+        this.hisName = _hisName;
+        this.hisPoints = _hisPoints;
+        this.hisDate = _hisDate;
+    }
+}
 
 
 
 
 
-/**************************** ouverture de l'alerte et départ du jeu DEBUT *********************************************/
-myH1.addEventListener("click", startGame);
-///////////////////////////////FONCTION DEPART DU JEU DEBUT
-function startGame() {    
-    alert("Are you ready ?");
-    myH1.removeEventListener("mouseenter", myHover);
-    myH1.removeEventListener("click", startGame);
-    document.querySelector("#ecran H3").style.display = "none";
-    let myDisplays = document.querySelectorAll(".datas");
-    myDisplays.forEach (function(x) {
-        x.style.display="block";
-    }); 
-    myCatch.classList.add("rotating");
-    myCatch.addEventListener("mouseover", catchMove);  
-    myTimer();
-    myEcran.addEventListener("click", myClicks);
-    myCatch.addEventListener("click", myWins)
-};
-///////////////////////////////FONCTION DEPART DU JEU FIN
-/**************************** ouverture de l'alerte et départ du jeu FIN *********************************************/
+/**************************** hight scores 5 premiers FIN *********************************************/
+
+
+
+
 
 /**************************** new game DEBUT *********************************************/
-document.querySelector(".new-game").addEventListener("click", newGame);
 function newGame() {
-    //clearInterval
-    //temps = 3600;
+    myTest = 15;
+    clearInterval(chrono);
+    C.mn.DOM.innerHTML = "60";
     document.querySelector("#ecran H3").innerHTML = "CLICK ON TITLE TO BEGIN";
     let stars = document.querySelectorAll(".stars");
     stars.forEach (function(x){
@@ -222,12 +312,18 @@ function newGame() {
     myHover()
     myH1.addEventListener("mouseenter", myHover);
     myH1.addEventListener("click", startGame);
+    
+    myWinCpt = 0;
+    myScoreCpt = 0;
+    myLevelCpt = 1;
+    myMissedClicksCpt = 0;
+    clickCounter = 0;
+    clickWinCounter = 0;
+    myPointsCpt = 10;
+    myPoints.innerHTML = `${myPointsCpt}`;
+    myLevel.innerHTML = `${myLevelCpt}`;
+    myScore.innerHTML = `${myScoreCpt}`; 
+    myMissedClicks.innerHTML = `${myMissedClicksCpt}`;
 
-
-    // myWinCpt = 0;
-    // myScoreCpt = 0;
-    // myLevelCpt = 1;
-    // myMissedClicksCpt = 0;
-    // clickCounter = 0;
 }
-/**************************** new game DEBUT *********************************************/
+/**************************** new game FIN *********************************************/
